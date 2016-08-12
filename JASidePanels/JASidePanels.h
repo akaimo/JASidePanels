a154cd36566960adb748465807e1bd4adb08119f
shuhei <kawashu0723@gmail.com>
2016-08-12 16:09:00 +0900

    carthage

---
+++ JASidePanels/JASidePanels.h
@@ -0,0 +1,20 @@
+//
+//  JASidePanels.h
+//  JASidePanels
+//
+//  Created by Shuhei Kawaguchi on 2016/08/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JASidePanels.
+FOUNDATION_EXPORT double JASidePanelsVersionNumber;
+
+//! Project version string for JASidePanels.
+FOUNDATION_EXPORT const unsigned char JASidePanelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JASidePanels/PublicHeader.h>
+
+#import "JASidePanelController.h"
+#import "UIViewController+JASidePanel.h"
